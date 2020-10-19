ea21f87f7f43593a8d5da28db4c90ac0f61bdc17
Anton Kamenetskiy <lemaxone@gmail.com>
2020-10-19 23:42:28 +0300

    feat: create morse-decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let reduceStr = expr.split('');
+    for (let i = 0; i < expr.length / 10 ; i++) {
+        let shift = 10 * i;
+        let subStr = reduceStr.slice(shift, shift + 10);
+        if (subStr[0] === '*') {
+            result += ' ';
+        }
+        else {
+            let padding = 0;
+            for (let j = 0; subStr[j] === '0'; j++) {
+                padding = j;
+            }
+            let morseChar = '';
+            let morse = '';
+            if (padding > 0) {
+                for (let k = padding; k < subStr.length - 2 ; k = k + 2) {
+                    morseChar = subStr[k + 1] + subStr[k + 2];
+                    if (morseChar === '10') morse += '.';
+                    if (morseChar === '11') morse += '-';
+                }
+            }
+            else {
+                for (let k = padding; k < subStr.length; k = k + 2) {
+                    morseChar = subStr[k] + subStr[k + 1];
+                    if (morseChar === '10') morse += '.';
+                    if (morseChar === '11') morse += '-';
+                }
+            }       
+            for (let h in MORSE_TABLE) {
+                if (morse === h) {
+                    result += MORSE_TABLE[h];
+                }
+            }
+        }
+    }
+return result;
 }
 
 module.exports = {
